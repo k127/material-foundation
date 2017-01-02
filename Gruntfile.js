a6d5c4fbbeb06beed0717b1d263bc5631ddcdb78
user <user@user>
2017-01-02 02:50:02 +0100

    add release

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     sass: {                              // Task
@@ -36,19 +36,45 @@ module.exports = function(grunt) {
         tasks: ['uglify'],
         options: {
           interrupt: true,
-        },
+        }
       },
       css: {
         files: '**/*.scss',
         tasks: ['sass'],
         options: {
           livereload: true,
-        },
-      },
+        }
+      }
     },
+    release: {
+      options: {
+        bump: true, //default: true
+        changelog: false, //default: false
+        changelogText: '<%= version %>\n', //default: '### <%= version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n'
+        file: 'package.json', //default: package.json
+        add: false, //default: true
+        commit: false, //default: true
+        tag: false, //default: true
+        push: false, //default: true
+        pushTags: false, //default: true
+        npm: false, //default: true
+        npmtag: true, //default: no tag
+        indentation: '\t', //default: '  ' (two spaces)
+        folder: 'dist', //default project root
+        tagName: '<%= version %>', //default: '<%= version %>'
+        commitMessage: 'check out my release <%= version %>', //default: 'release <%= version %>'
+        tagMessage: 'tagging version <%= version %>', //default: 'Version <%= version %>',
+        beforeBump: [], // optional grunt tasks to run before file versions are bumped
+        afterBump: [], // optional grunt tasks to run after file versions are bumped
+        beforeRelease: [], // optional grunt tasks to run after release version is bumped up but before release is packaged
+        afterRelease: [], // optional grunt tasks to run after release is packaged
+        updateVars: [], // optional grunt config objects to update (this will update/set the version property on the object specified)
+      }
+    }
   });
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['sass','uglify']);
+  grunt.loadNpmTasks('grunt-release');
+  grunt.registerTask('default', ['sass', 'uglify']);
 };
